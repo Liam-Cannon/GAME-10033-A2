579610514ca27032bfa16737890959022fc77575
bonni <bonni@DESKTOP-2RT9N00>
2026-09-23 15:44:28 -0400

    Update Game.cs

--- Cannon-Liam-A2-project(2)/Game.cs
+++ Cannon-Liam-A2-project(2)/Game.cs
@@ -15,7 +15,10 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            Window.ClearBackground(Color.Black);
+            Window.SetSize(400, 400);
+            Window.SetTitle("Undertale Soul Box");
+            
         }
 
         /// <summary>
@@ -23,6 +26,49 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            //Declare variables for position of the circle
+
+            float soulX;
+            float soulY;
+
+            float inputX = 0;
+            float inputY = 0;
+
+           // Draw.SetFillColor(Color.Red);
+           // Draw.Circle(200, 200, 25);
+            // Using module 1.4 as a baseline, testing if movement works
+            if (Input.IsKeyboardKeyDown(KeyboardKey.W))
+            {
+                inputY -= 1;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardKey.S))
+            {
+                inputY += 1;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardKey.A))
+            {
+                inputX -= 1;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardKey.D))
+            {
+                inputX += 1;
+            }
+
+            soulX += inputX * 100 * Time.DeltaTime;
+            soulY += inputY * 100 * Time.DeltaTime;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
